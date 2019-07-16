5165a2d0dd5f9e4df1afbff432be3c2b7b326afa
suganthicj <52270901+suganthicj@users.noreply.github.com>
2019-07-15 22:55:42 -0700

    Create tani.py

---
+++ tani.py
@@ -0,0 +1,21 @@
+print(fibonacci(9)) 
+  
+#  
+def fibonacci(n): 
+    a = 0
+    b = 1
+    if n < 0: 
+        print("Incorrect input") 
+    elif n == 0: 
+        return a 
+    elif n == 1: 
+        return b 
+    else: 
+        for i in range(2,n): 
+            c = a + b 
+            a = b 
+            b = c 
+        return b 
+  
+# Driver Program 
+  This code is contributed by Saket Modi 
